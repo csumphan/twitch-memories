ffaa772936a61d38e9421e879f35b019decd9fa0
Elton Xue <eltonxue@gmail.com>
2019-09-30 15:33:18 -0700

    Configured SCSS into webpack and organized file structure for SCSS

--- src/LiveConfig.js
+++ src/LiveConfig.js
@@ -1,8 +1,6 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import LiveConfigPage from "./components/LiveConfigPage/LiveConfigPage"
+import React from "react";
+import ReactDOM from "react-dom";
+import LiveConfigPage from "./components/LiveConfigPage/LiveConfigPage";
+import "./scss/main";
 
-ReactDOM.render(
-  <LiveConfigPage />,
-  document.getElementById("root")
-)
+ReactDOM.render(<LiveConfigPage />, document.getElementById("root"));
